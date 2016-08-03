5eb12188058f347b0e0b75ab7627acfdce797ce6
TooKyA <lhuac415@gmail.com>
2016-08-03 09:44:42 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# flarum-ext-mediaembed-https
-自动识别第三方网站的媒体（图片、视频、音频）并内嵌到 Flarum 。（https版本）
+# 介绍
+
+自动识别第三方网站的媒体（图片、视频、音频）并内嵌到 Flarum （https版本）。
+
+# 安装
+
+```
+composer require c78/flarum-ext-mediaembed-https
+```
+# 自动识别图片链接
+
+- 直接插入图片链接即可。
+
+# 支持自动识别媒体的网站
+
+- 网易云音乐
+- 腾讯视频
+- bilibili
+
+# 一些问题
+
+- 如果你是非https的站点，推荐安装：[https://github.com/c78/flarum-ext-mediaembed](https://github.com/c78/flarum-ext-mediaembed) :-)。
+
+- 有问题或提交新的站点支持？请提交issues :-)。
+
+# 支持
+
+- [https://www.e7ko.com/](https://www.e7ko.com/)
+
+# 开发参考
+
+[https://github.com/s9e/flarum-ext-mediaembed/](https://github.com/s9e/flarum-ext-mediaembed/)
+
+[https://github.com/s9e/TextFormatter/blob/master/docs/Plugins/MediaEmbed/Add_custom.md](https://github.com/s9e/TextFormatter/blob/master/docs/Plugins/MediaEmbed/Add_custom.md)
+
---
+++ bootstrap.php
@@ -0,0 +1,60 @@
+<?php 
+namespace c78\MediaEmbed;
+use Flarum\Event\ConfigureFormatter;
+use Illuminate\Events\Dispatcher;
+use s9e\TextFormatter\Configurator\Bundles\MediaPack;
+
+function subscribe(Dispatcher $events)
+{
+    $events->listen(
+        ConfigureFormatter::class,
+        function (ConfigureFormatter $event)
+        {
+            $event->configurator->Autoimage;
+            $event->configurator->MediaEmbed->add(
+                'music163',
+                [
+                    'host'    => 'music.163.com',
+                    'extract' => "!music\\.163\\.com/#/song\\?id=(?'id'\\d+)!",
+                    'iframe'  => [
+                        'width'  => 450,
+                        'height' => 86,
+                        'src'    => '//music.163.com/outchain/player?type=2&id={@id}&auto=0&height=66'
+                    ]
+                ]
+            );
+            $event->configurator->MediaEmbed->add(
+                'bilibili',
+                [   
+                    'host'    => 'www.bilibili.com',
+                    'extract' => "!www.bilibili.com/video/av(?'id'\\d+)/!",
+                    'flash'  => [
+                        'width'  => 760,
+                        'height' => 450,
+                        'src'    => 'https://static-s.bilibili.com/miniloader.swf?aid={@id}'
+                    ]
+                ]
+            );
+             $event->configurator->MediaEmbed->add(
+                'qq',
+                [
+                    'host'    => 'qq.com',
+                    'extract' => [
+                       "!qq\\.com/x/cover/\\w+/(?'id'\\w+)\\.html!",
+                       "!qq\\.com/x/cover/\\w+\\.html\\?vid=(?'id'\\w+)!",
+                       "!qq\\.com/cover/[^/]+/\\w+/(?'id'\\w+)\\.html!",
+                       "!qq\\.com/cover/[^/]+/\\w+\\.html\\?vid=(?'id'\\w+)!"
+                    ],
+                    'iframe'  => [
+                        'width'  => 760,
+                        'height' => 450,
+                        'src'    => '//v.qq.com/iframe/player.html?vid={@id}&tiny=0&auto=0'
+                    ]
+                ]
+            );
+            (new MediaPack)->configure($event->configurator);
+        }
+    );
+};
+
+return __NAMESPACE__ . '\\subscribe';
